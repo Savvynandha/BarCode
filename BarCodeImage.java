9c17ec8dfeda8dd1b6a220c956f9afe56c8b0d2b
CDunba <courtneydunbar336@yahoo.com>
2017-09-21 21:15:52 -0700

    Update BarCodeImage.java

--- BarCodeImage.java
+++ BarCodeImage.java
@@ -1,5 +1,15 @@
 
 public class BarCodeImage {
+   public static final int MAX_HEIGHT = 30;    
+   public static final int MAX_WIDTH = 65;
+   
+   private boolean[][] image_data {
+	   /*
+	    * This is where to store your image.  If the incoming data is smaller than the max, instantiate memory anyway, 
+	    * but leave it blank (white). This data will be false for elements that are white, and true for elements that are 
+	    * black.
+	    */
+   }
    BarCodeImage(){
 	   
    }
