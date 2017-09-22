ddd0caeb379f7c8a75bb91958f3a471915b8e943
CDunba <courtneydunbar336@yahoo.com>
2017-09-21 21:22:11 -0700

    Update BarCodeImage.java

--- BarCodeImage.java
+++ BarCodeImage.java
@@ -11,6 +11,18 @@ public class BarCodeImage {
 	    */
    }
    BarCodeImage(){
-	   
+	   BarCodeImage[][] bc= new BarCodeImage[1][];
+      //instantiates a 2D array (MAX_HEIGHT x MAX_WIDTH) and stuffs it all with blanks (false).	   
+	   //null loop
    }
+
+   BarcodeImage(String[] str_data){
+	   /*-takes a 1D array of Strings and converts it to the internal 2D array of booleans. This constructor is a little 
+	    * tricky because the incoming image is not the necessarily same size as the internal matrix.  So, you have to pack 
+	    * it into the lower-left corner of the double array, causing a bit of stress if you don't like 2D counting.  
+	    * This is good 2D array exercise.  The DataMatrix class will make sure that there is no extra space below or left 
+	    * of the image so this constructor can put it into the lower-left corner of the array.
+	    */
+	   
+}
 }
